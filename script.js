754748f7a59910d740be7a0a5991f19a8deab565
C Bendix <christopherbendix@gmail.com>
2018-01-04 20:03:48 -0800

    updated html and js files

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+<script type="text/javascript">
 let firstName = "Chris";
 let lastName = "Bendix";
 let yearsOfStudy = 0;
 let goal = "to complete the Programming course of study with SCCC";
+</script>
