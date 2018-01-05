1d65056c522dc1fc191151629e92dd94abf2a268
C Bendix <christopherbendix@gmail.com>
2018-01-04 21:06:03 -0800

    updated html and js files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstName = "Chris";
-let lastName = "Bendix";
-let yearsOfStudy = 0;
-let goal = "explore software development";
+var firstName = "Chris";
+var lastName = "Bendix";
+var yearsOfStudy = 0;
+var goal = "explore software development";
 document.write("My name is " + firstName lastName + " . I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses.");
